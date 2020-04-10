5c4319effabb758314a06249116cd1e62f68f9e6
codersantosh <codersantosh@gmail.com>
2020-04-10 20:23:50 +0545

    added breadcrumb for gutentor template too

--- inc/hooks/breadcrumb.php
+++ inc/hooks/breadcrumb.php
@@ -55,26 +55,9 @@ if (!function_exists('cosmoswp_before_content_breadcrumb_structure')):
         $breadcrumb_options = cosmoswp_get_theme_options('cosmoswp-breadcrumb-options');
         $before_content     = cosmoswp_get_theme_options('breadcrumb-before-content');
         if ('disable' != $breadcrumb_options && $before_content) {
-            echo '<div class="grid-container">';
-            if (!is_front_page()) {
-                if (function_exists('yoast_breadcrumb') && 'yoast' == $breadcrumb_options) {
-                    echo "<div class='breadcrumbs init-animate'><div id='cwp-breadcrumbs' class='cwp-breadcrumbs'>";
-                    yoast_breadcrumb();
-                    echo "</div></div>";
-                } else if (function_exists('bcn_display') && 'bcn' == $breadcrumb_options) {
-                    echo "<div class='breadcrumbs init-animate'><div id='cwp-breadcrumbs' class='cwp-breadcrumbs'>";
-                    bcn_display();
-                    echo "</div></div>";
-                } else if (class_exists('WooCommerce') && 'woocommerce' == $breadcrumb_options) {
-                    echo "<div class='breadcrumbs init-animate'><div id='cwp-breadcrumbs' class='cwp-breadcrumbs'>";
-                    woocommerce_breadcrumb();
-                    echo "</div></div>";
-                }
-            }
-            echo '</div>';
+            cosmoswp_breadcrumb_structure();
         }
     }
 
     add_action('gutentor_template_before_loop', 'cosmoswp_before_content_breadcrumb_structure');
 endif;
-
